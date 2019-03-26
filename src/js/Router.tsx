15dbd54ee78fb8da2b16aadd9210c9e467280032
S3Ekin <sekin4866111@163.com>
2019-03-26 22:13:10 +0800

    把jsx和js混着写，又是当组件又是当函数调用，我这是不是高阶组件，我还是不太明白，不过可以再多研究研究

--- src/js/Router.tsx
+++ src/js/Router.tsx
@@ -2,31 +2,86 @@ import {Route,Switch} from "react-router";
 import {Link,BrowserRouter} from "react-router-dom";
 import * as React from "react";
 import App from "./index";
+import Menu from "./common/Menu" ;
 
+
+const ThemeContext = React.createContext("dark");
+
+const Theme = ()=>(
+
+		<ThemeContext.Consumer >
+			{theme=> <button>{theme}</button>}
+		</ThemeContext.Consumer>
+
+	);
+
+const data = [
+	{
+		text:"1",
+		id:"1",
+			url:"aaa",
+		children:[
+			{
+					text:"1-1",
+					id:"1-1",
+					children:[],
+					url:"aaa",
+			}
+		],
+	},
+	{
+		text:"2",
+		id:"2",
+		url:"aaa",
+		children:[
+			{
+					text:"2-1",
+					id:"2-1",
+					children:[
+						{
+							text:"2-2-1",
+							id:"2-2",
+								url:"aaa",
+							children:[
+								{
+										text:"1-1",
+										id:"1-1",
+										children:[],
+										url:"aaa",
+								}
+							],
+						},
+					],
+					url:"aaa",
+			}
+		],
+	}
+];
 
 const About = ()=>(
-
+			<ThemeContext.Provider value="d">
 			<div>
 					<p>
 						我是About
 					</p>
 					<div>
-						学习路由
+						<Menu data={data} config={{}} />
+						<Theme />
 					</div>
 			</div>
-
+			</ThemeContext.Provider >
 	);
 
 const Nav = ()=>(
 			 <div>
 					<Link to="/todo" > tdos </Link>
-					<Link to="/about" >Aasdbout</Link>
+					<Link to="/about" >Aaddddsout</Link>
 				</div> 
 	);
 
 
 const Page = ()=>(
-						<BrowserRouter basename="/asdf">
+						<BrowserRouter basename="asdf">
 							<Nav/>
 							<Switch>
 										  <Route path="/todo" component={App} />
